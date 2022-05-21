bc733251b8bdf76801f68b5fca7d1285ad8b992f
Jan Carlos Charris <jancarlosch@outlook.es>
2022-05-20 20:43:13 -0500

    Updates

---
+++ precache-manifest.bdc724fab1c8c73fc2f6a6be19a9a1ff.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5b08cfbc94370e25291",
+    "url": "/crud-firebase/static/js/main.b5b08cfb.chunk.js"
+  },
+  {
+    "revision": "243630fe7ac7e84a2411",
+    "url": "/crud-firebase/static/js/1.243630fe.chunk.js"
+  },
+  {
+    "revision": "07476ee53417b8215e16",
+    "url": "/crud-firebase/static/js/runtime~main.07476ee5.js"
+  },
+  {
+    "revision": "f5186e3efe86484ace5e4a6bba8aa19f",
+    "url": "/crud-firebase/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/JancarlosCh/crud-firebase/precache-manifest.031bc1ce6dd3f239a991d8897980eaa5.js"
+  "/crud-firebase/precache-manifest.bdc724fab1c8c73fc2f6a6be19a9a1ff.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/JancarlosCh/crud-firebase/index.html", {
+workbox.routing.registerNavigationRoute("/crud-firebase/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
